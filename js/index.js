ca869a415e7e07360f26f798e4832564e8942f65
jishanprodhan12 <mdjishanprodhan132@gmail.com>
2024-10-09 03:25:21 -0500

    btn active done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
-const showCategories = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories')
-    const data = await res.json()
-    displayCategories(data.categories);
+const showCategories =() => {
+     fetch('https://openapi.programming-hero.com/api/peddy/categories')
+    .then((res)=> res.json())
+    .then((data)=> {
+        displayCategories(data.categories);
+
+       
+       
+
+    })
+    .catch((error)=>console.log(error));
+
+  
+}
+
+// remove active
+const removeActiveClass = () => {
+    const RemoveBtn = document.getElementsByClassName('catagorie-active');
+    for (const btn of RemoveBtn) {
+        btn.classList.remove('active' , 'rounded-lg','bg-transparent');
+    }
+
 }
 // all pets show 
 const allPets = async () => {
@@ -11,6 +29,7 @@ const allPets = async () => {
 
 }
 
+
 const displayCategories = (data) => {
     //target div 
     const categoriesContainer = document.getElementById('categories-btn-container');
@@ -19,7 +38,7 @@ const displayCategories = (data) => {
         const categorieBtn = document.createElement('button');
         const { id, category, category_icon } = item;
         categorieBtn.innerHTML = `
-                    <button class="btn btn-lg  border-2 bg-transparent hover:bg-transparent flex rounded-lg shadow-md gap-2  " onclick="showEachCategorie('${category}')">
+                    <button class="btn btn-lg  border-2 bg-transparent hover:bg-transparent flex rounded-lg shadow-md gap-2  overflow-hidden catagorie-active " onclick="showEachCategorie('${category}')" id="categorie-btn${category}">
                         <span class="w-12 h-12">
 
                             <img src="${category_icon}" alt="" class="w-full">
@@ -29,23 +48,10 @@ const displayCategories = (data) => {
         `
         categoriesContainer.appendChild(categorieBtn);
     });
+    
 }
 
-
-// {
-//     "petId": 16,
-//     "breed": "English Angora",
-//     "category": "Rabbit",
-//     "date_of_birth": "2023-08-05",
-//     "price": 300,
-//     "image": "https://i.ibb.co.com/zZHPJNh/pet-16.jpg",
-//     "gender": "Female",
-//     "pet_details": "This fluffy female English Angora rabbit, born on August 5, 2023, is known for her long, luxurious fur. Priced at $300, she's perfect for families who enjoy grooming and cuddling. She is not vaccinated.",
-//     "vaccinated_status": "Not",
-//     "pet_name": "Snowball"
-// }
-
-const showAllPet = (pets) => {
+showAllPet = (pets) => {
     const petCardContainer = document.getElementById('pet-card-cotainer');
     pets.forEach(pet => {
         const petCard = document.createElement('div');
@@ -114,6 +120,10 @@ const addLikeContainer = (likedImg) => {
 
 
 const showEachCategorie = async (eachPet) => {
+    removeActiveClass();
+    
+    const addActive = document.getElementById(`categorie-btn${eachPet}`);
+    addActive.classList.add('active');
     const spinerShow = document.getElementById('spiner');
     document.getElementById('pet-card-cotainer').innerText = "";
     spinerShow.classList.remove('hidden');
@@ -128,7 +138,6 @@ const showEachCategorie = async (eachPet) => {
     console.log("2 min waste");
 }
 const displayEachPet = (pet) => {
-    console.log(pet);
     const petCardContainer = document.getElementById('pet-card-cotainer');
     petCardContainer.classList.add("grid");
     petCardContainer.innerHTML = '';
@@ -196,7 +205,7 @@ const displayEachPet = (pet) => {
             </div>
             <div >
                 <h3 class=" md:text-3xl font-extrabold w-full lg:w-fit text center mx-auto my-2">No Information Available</h3> 
-                <p class="text-sm text-gray-500 w-full lg:w-3/4 mx-auto text-center my-2">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a.</p>
+                <p class="text-sm text-gray-500 w-full lg:w-3/4 mx-auto text-center my-2">It seems the bird you're searching for cannot be found. Please double-check the name or try a different search term. Our collection may not currently include this bird, but feel free to explore other categories!</p>
             </div>
         </div>
     
@@ -207,11 +216,17 @@ const displayEachPet = (pet) => {
 }
 
 const showPetDetails = async (id) => {
+   try{
     const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`);
     const data = await res.json();
     showPetModal(data.petData);
-
     my_modal_5.showModal();
+    
+   }catch(error){
+    console.log(error);
+   }
+   
+ 
 }
 
 const showPetModal = (Details) => {
@@ -230,7 +245,7 @@ const showPetModal = (Details) => {
                             <!-- text  -->
                              <div class="my-2">
                     
-                                <h3>${(pet_name === undefined ? ' not Available' : pet_name)}</h3>
+                                <h3 class="text-xl font-bold">${(pet_name === undefined ? ' not Available' : pet_name)}</h3>
                                 <!-- breed gender container -->
                                 <div class="grid grid-cols-2 gap-1 md:gap-3 my-3">
                                     <p class="flex items-center gap-2">
@@ -265,20 +280,19 @@ const showPetModal = (Details) => {
 
                         </div>
                         <!-- close model btn -->
-                        <div class="w-full border-2">
+                       
                             <form method="dialog">
                                 <!-- if there is a button in form, it will close the modal -->
                                 <button
                                     class="btn bg-lime-50 w-full text-primary-color text-xl font-bold">Close</button>
                             </form>
-                        </div>
+                        
                     </div>
                 </dialog>
             </div>
     
     `
     DetailsModalContainer.appendChild(detailsModal);
-
 }
 showCategories();
 allPets();
